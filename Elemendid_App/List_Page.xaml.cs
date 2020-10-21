c00866d21bf7306428524c92dca56099b88e6951
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2020-10-21 12:51:56 +0300

    Add project files.

---
+++ Elemendid_App/List_Page.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elemendid_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class List_Page : ContentPage
+    {
+        public List_Page()
+        {
+            string[] tasks = new string[] { "Tõusen püsti", "Söön putru", "Jalutan", "Lähen lõunale", "Tegelen spordiga", "Söön", "Magan" };
+
+            ListView list = new ListView();
+            // определяем источник данных
+            list.ItemsSource = tasks;
+            Content = new StackLayout { Children = { list } };
+        }
+    }
+}
---
+++ Elemendid_App/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Elemendid_App
+{
+    public partial class MainPage : ContentPage
+    {
+        Button[] btns = new Button[3];
+        String[] btn_Text = new String[] {"Table Page", "Web Page", "ListView Page" };
+        StackLayout st;
+        public MainPage()
+        {
+            st = new StackLayout();
+            for (int i = 0; i < 3; i++)
+            {
+                btns[i] = new Button { Text = btn_Text[i] };
+                st.Children.Add(btns[i]);
+                btns[i].Clicked += Btn_Clicked;
+            }           
+            Content = st;
+
+            //Button Tbl_btn = new Button { Text = "Table Page" };
+            //Button Web_btn = new Button { Text = "Web Page" };
+            //Button List_btn = new Button { Text = "ListView Page" };
+            //st.Children.Add(Tbl_btn);
+            //st.Children.Add(Web_btn);
+            //st.Children.Add(List_btn);
+            //Tbl_btn.Clicked += new EventHandler(Btn_Clicked);4
+            //Web_btn.Clicked += new EventHandler(Btn_Clicked);4
+            //List_btn.Clicked += new EventHandler(Btn_Clicked);4
+            //Tbl_btn.Clicked += Tbl_btn_Clicked;3
+            //Content = st;
+            
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            switch (btn.Text) // I want to get the name of the control 
+            {
+                case "Table Page":
+                    await Navigation.PushAsync(new Table_Page());
+                    break;
+                case "Web Page":
+                    await Navigation.PushAsync(new Web_Page());
+                    break;
+                case "ListView Page":
+                    await Navigation.PushAsync(new List_Page());
+                    break;
+            }
+        }
+
+        /*private async void Tbl_btn_Clicked(object sender, EventArgs e)3
+        {
+            await Navigation.PushAsync(new Table_Page());
+        }*/
+
+    }
+}
